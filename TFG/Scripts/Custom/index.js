23f772a858be7eaf5a5ffc13b5205567e8c22081
itsRequena <danielrequena0602@gmail.com>
2023-07-09 18:34:13 +0200

    Cambios formulario

---
+++ TFG/Scripts/Custom/index.js
@@ -0,0 +1,21 @@
+﻿function clearText(element) {
+    if (element.value === "") {
+        element.placeholder = "";
+    }
+}
+
+function restoreTextU(element) {
+    if (element.value === '') {
+        element.placeholder = "Nombre de usuario";
+    }
+}
+
+function restoreTextP(element) {
+    if (element.value === '') {
+        element.placeholder = "Contraseña";
+    }
+}
+
+
+
+
